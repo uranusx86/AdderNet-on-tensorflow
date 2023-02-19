e857e7a2e1836d81fcf1359eeff5b4802476518f
uranusx86 <uranusx86@hotmail.com>
2023-02-19 18:56:41 +0800

    add adaptive gradient scaling, increase the epoches and change to NAG optimizer

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ At first, the training accuracy was 96% and the testing accuracy was 95.6% on th
 
 [2023-02-03] add full-precision gradient, the training accuracy is 97.4% and the testing accuracy is 96.9% on the MNIST dataset.
 
-[2023-02-04] add gradient clipping, the training accuracy is 97.1% and  the testing accuracy is 96.4% on the MNIST dataset.
+[2023-02-04] add gradient clipping, the training accuracy is 97.1% and the testing accuracy is 96.4% on the MNIST dataset.
 
-[2023-02-16] add cosine lr, the training accuracy is 99.97% and  the testing accuracy is 98.87% on the MNIST dataset.
+[2023-02-16] add cosine lr, the training accuracy is 99.97% and the testing accuracy is 98.87% on the MNIST dataset.
+
+[2023-02-18] add adaptive gradient scaling, change the optimizer from adam to NAG and increase the epoches to 50, the training accuracy is 99.99% and the testing accuracy is 98.94% on the MNIST dataset.
--- mnist_example.py
+++ mnist_example.py
@@ -1,6 +1,52 @@
 import tensorflow as tf
 
-from custom_conv import L1Conv2D, L1Dense
+from L1_layers import L1Conv2D, L1Dense
+
+class GradientClippingModel(tf.keras.Model):
+    def compile(
+        self,
+        optimizer="rmsprop",
+        loss=None,
+        metrics=None,
+        loss_weights=None,
+        weighted_metrics=None,
+        run_eagerly=None,
+        steps_per_execution=None,
+        jit_compile=None,
+        **kwargs,
+    ):
+        super().compile(optimizer, loss, metrics, loss_weights, weighted_metrics, run_eagerly, steps_per_execution, jit_compile, **kwargs)
+        self.multiplier = {}
+        for var in self.trainable_variables:
+            if "conv2d" in var.name:
+              self.multiplier[ var.name ] = 0.1*np.sqrt(np.product(var.shape))
+            elif "dense" in var.name:
+              self.multiplier[ var.name ] = 0.1*np.sqrt(np.product(var.shape))
+            elif "batch_normalization" in var.name:
+              self.multiplier[ var.name ] = 1.0
+            else:
+              raise ValueError("layer name can't recognize, found {}".format(var.name))
+
+    def train_step(self, data):
+        x, y = data
+        with tf.GradientTape() as tape:
+            # Forward pass
+            y_pred = self(x, training=True)
+            # Compute the loss value
+            # (the loss function is configured in `compile()`)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # adaptive scaling
+        gradients = [grad / tf.norm(grad, ord=2) * self.multiplier[ trainable_vars[idx].name ] for idx,grad in enumerate(gradients)]
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Update metrics (includes the metric that tracks the loss)
+        self.compiled_metrics.update_state(y, y_pred)
+        # Return a dict mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
 
 def main():
     # dataset
@@ -38,20 +84,21 @@ def main():
     lenet_5bn.summary()
 
     # build model
+    custom_model = GradientClippingModel(inputs=lenet_5bn.inputs, outputs=lenet_5bn.outputs)
     loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     # epoches*step_per_epoch
-    epochs = 20
+    epochs = 50
     batch_size = 256
     lr = tf.keras.optimizers.schedules.CosineDecay(initial_learning_rate=0.1, decay_steps=epochs*np.ceil(len(x_train)/batch_size).astype(np.int32))
-    lenet_5bn.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr, clipvalue=1.0),
+    custom_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=lr, momentum=0.9, nesterov=True, weight_decay=5e-4, clipvalue=1.0),
             loss=loss_fn,
             metrics=['accuracy'])
 
     # training
-    lenet_5bn.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+    custom_model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
 
     # validation
-    lenet_5bn.evaluate(x_test,  y_test, verbose=2)
+    custom_model.evaluate(x_test,  y_test, verbose=2)
 
 if __name__ == '__main__':
     main()
